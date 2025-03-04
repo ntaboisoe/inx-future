3ea182b71c15d8e355f4e89fb76a0c062cf96fd3
Ntabo Isoe <intaborobert@gmail.com>
2025-03-04 12:04:40 +0300

    Update home.py

--- home.py
+++ home.py
@@ -8,7 +8,13 @@ st.title("INX Future Inc. - Project Summary")
 st.markdown("### Employee Performance Prediction Project")
 st.markdown("This app is designed to predict employee performance ratings based on various features. "
             "It uses a Random Forest model trained on employee data. Below are the main sections of this application:")
-st.markdown("* **Main Page:** Provides an overview of the project, source data analysis, and visualizations.")
-st.markdown("* **Prediction Model Page:** Allows users to upload their own data to predict employee performance ratings.")
+st.markdown("""* **Main Page:** [Provides an overview of the project, source data analysis, and visualizations.](https://inx-future-app-ntaboisoe.streamlit.app/main)""")
+st.markdown("""* **Prediction Model Page:** [Allows users to upload their own data to predict employee performance ratings.](https://inx-future-app-ntaboisoe.streamlit.app/prediction_model)""")
+#st.markdown("* **Main Page:** Provides an overview of the project, source data analysis, and visualizations.")
+#st.markdown("* **Prediction Model Page:** Allows users to upload their own data to predict employee performance ratings.")
+
 st.markdown("---")
 st.markdown("This project leverages data science techniques for HR analytics, aiming to provide insights into employee performance factors and facilitate data-driven decision-making.")
+
+
+
